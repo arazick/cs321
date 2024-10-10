e1072fce3563075420a67475e7d0d838f6cdc271
Nyle Nasir <nylesalman@gmail.com>
2024-10-10 09:39:50 -0400

    Added 3 methods that return true

--- newimm-domain/src/main/java/Immigrant.java
+++ newimm-domain/src/main/java/Immigrant.java
@@ -1,3 +1,13 @@
 public class Immigrant {
 
+    public Boolean isAvailable() {
+    return true;
+    }
+    public Boolean addForm() {
+        return true;
+    }
+    public Boolean updateForm() {
+        return true;
+    }
+
 }
