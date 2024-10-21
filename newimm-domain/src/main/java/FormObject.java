0cf5dc6223cecb43f3a3c49229e18192f81c33f4
arazick <azrazick@gmail.com>
2024-10-21 18:58:24 -0400

    Final Changes

--- newimm-domain/src/main/java/FormObject.java
+++ newimm-domain/src/main/java/FormObject.java
@@ -5,35 +5,39 @@ public class FormObject {
     private String deceasedDOB;
     private String applicantID;
     private String deceasedID;
-public FormObject(String applicant, String deceased, String applicantDOB, String deceasedDOB, String applicantID, String deceasedID){
-    this.applicant = applicant;
-    this.deceased = deceased;
-    this.applicantDOB = applicantDOB;
-    this.deceasedDOB = deceasedDOB;
-    this.applicantID = applicantID;
-    this.deceasedID = deceasedID;
-}
-public String getApplicant() {
-    return applicant;
-}
 
-public String getDeceased() {
-    return deceased;
-}
+    public FormObject(String applicant, String deceased, String applicantDOB, String deceasedDOB, String applicantID,
+            String deceasedID) {
+        this.applicant = applicant;
+        this.deceased = deceased;
+        this.applicantDOB = applicantDOB;
+        this.deceasedDOB = deceasedDOB;
+        this.applicantID = applicantID;
+        this.deceasedID = deceasedID;
+    }
 
-public String getApplicantDOB() {
-    return applicantDOB;
-}
+    public String getApplicant() {
+        return applicant;
+    }
 
-public String getDeceasedDOB() {
-    return deceasedDOB;
-}
-public String getApplicantID() {
-    return applicantID;
-}
+    public String getDeceased() {
+        return deceased;
+    }
 
-public String getDeceasedID() {
-    return deceasedID;
-}
+    public String getApplicantDOB() {
+        return applicantDOB;
+    }
+
+    public String getDeceasedDOB() {
+        return deceasedDOB;
+    }
+
+    public String getApplicantID() {
+        return applicantID;
+    }
+
+    public String getDeceasedID() {
+        return deceasedID;
+    }
 
 }
