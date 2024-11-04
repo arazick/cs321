1365ddbbf002f6acfa6759edbeed00c82ee70798
ShaSamik <151794289+ShaSamik@users.noreply.github.com>
2024-11-04 16:54:30 -0500

    Update AuditPage.java

--- newimm-domain/src/main/java/edu/gmu/cs321/AuditPage.java
+++ newimm-domain/src/main/java/edu/gmu/cs321/AuditPage.java
@@ -9,7 +9,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
